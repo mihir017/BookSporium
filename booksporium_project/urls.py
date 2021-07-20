ba33a806ce4a3ea3297bbc30d8d31d0b5a00ce05
Mihir <web.mihir017@gmail.com>
2021-07-20 16:56:02 +0530

    deploy on heroku

--- booksporium_project/urls.py
+++ booksporium_project/urls.py
@@ -24,7 +24,9 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('accounts.urls')),
-    url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
+    url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
